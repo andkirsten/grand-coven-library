7d72e2cf9bcab2a66843547774a56d87d5ed4ce6
andkirsten <and.kirsten@gmail.com>
2025-07-15 22:57:55 -0500

    separate base.html for home and about pages

--- books/urls.py
+++ books/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('card/', views.card, name='card'),
+    path('about/', views.about, name='about'),
 ]
--- books/views.py
+++ books/views.py
@@ -19,3 +19,6 @@ def home(request):
 
 def card(request):
     return render(request, 'books/card.html')
+
+def about(request):
+    return render(request, 'books/about.html')
