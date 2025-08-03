47da4306d887cd9d54081efe65f807037f362663
andkirsten <and.kirsten@gmail.com>
2025-08-03 14:12:04 -0500

    fixing author code

--- books/utils.py
+++ books/utils.py
@@ -3,7 +3,12 @@ def generate_call_number(book_instance):
     from .models import Book
     last = (book_instance.author_last_name or '').strip()
     first = (book_instance.author_first_name or '').strip()
-    author_code = (last[:2] or first[:2]).upper() if (last or first) else 'XX'
+    
+    # If author is anonymous or no author name provided, use first two letters of title
+    if book_instance.author_anonymous or not (last or first):
+        author_code = book_instance.title[:2].upper() if book_instance.title else 'XX'
+    else:
+        author_code = (last[:2] or first[:2]).upper()
 
     last_entry = Book.objects.filter(
         magic_category=book_instance.magic_category,
@@ -19,7 +24,12 @@ def generate_call_number_from_id(book_instance):
     """Generate call number using the book's ID as the entry number"""
     last = (book_instance.author_last_name or '').strip()
     first = (book_instance.author_first_name or '').strip()
-    author_code = (last[:2] or first[:2]).upper() if (last or first) else 'XX'
+    
+    # If author is anonymous or no author name provided, use first two letters of title
+    if book_instance.author_anonymous or not (last or first):
+        author_code = book_instance.title[:2].upper() if book_instance.title else 'XX'
+    else:
+        author_code = (last[:2] or first[:2]).upper()
     
     entry_str = f"{book_instance.id:03d}"
     
